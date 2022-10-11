6362eefc1ac2fb697935921c6af5ab5f6431b022
Martin Dann <marvdann76@gmail.com>
2022-10-11 22:09:21 +0100

    fix video width

--- app/routes/techniques/$slug.tsx
+++ app/routes/techniques/$slug.tsx
@@ -80,7 +80,7 @@ export default function TechniqueDetailPage() {
       {data.technique.youtubeVideoId && (
         <div className="py-2 px-6 md:py-6">
           <iframe
-            style={{ width: '78vw', height: '42vw', margin: '0 auto' }}
+            style={{ width: '78vw', height: '44vw', margin: '0 auto' }}
             src={`https://www.youtube-nocookie.com/embed/${data.technique.youtubeVideoId}`}
             title="YouTube video player"
             frameBorder="0"
